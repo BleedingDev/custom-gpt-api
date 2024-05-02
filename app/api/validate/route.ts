24eb373f98a7b0fdc376685effb2da421d5e8c1d
Petr Glaser <syreanis+1@gmail.com>
2024-05-02 07:31:50 +0200

    feat(api): add JSON schema validation endpoint

---
+++ app/api/validate/route.ts
@@ -0,0 +1,54 @@
+import { TypedNextResponse, route, routeOperation } from "next-rest-framework";
+import { z } from "zod";
+import Ajv from "ajv";
+
+export const { POST } = route({
+  validateJson: routeOperation({
+    method: "POST",
+    openApiOperation: {
+      description: "Validate JSON against schema",
+    },
+  })
+    .input({
+      contentType: "application/json",
+      body: z.object({
+        json: z.string(),
+        schema: z.string(),
+      }),
+    })
+    .outputs([
+      {
+        status: 201,
+        contentType: "application/json",
+        body: z.string(),
+      },
+      {
+        status: 400,
+        contentType: "application/json",
+        body: z.string(),
+      },
+    ])
+    .handler(async (req) => {
+      try {
+        const { json, schema } = await req.json();
+
+        const ajv = new Ajv();
+
+        const validate = ajv.compile(schema as any);
+        const valid = validate(json);
+        if (!valid) {
+          return TypedNextResponse.json(JSON.stringify(validate.errors), {
+            status: 400,
+          });
+        }
+
+        return TypedNextResponse.json("Provided JSON is valid", {
+          status: 201,
+        });
+      } catch (error) {
+        return TypedNextResponse.json(`Error: ${error}`, {
+          status: 400,
+        });
+      }
+    }),
+});
